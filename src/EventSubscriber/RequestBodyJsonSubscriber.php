92038c68a3eacdfaf92635ef518eefb370e8ef82
Marat Hovsepyan <marathovsepyan45@gmail.com>
2018-11-11 19:13:36 +0400

    Create event subscriber for accepting json request body
    `RequestBodyJsonSubscriber` event subscriber is created for listening
    to requests with "application/json" content type, decoding request data
    and setting decoded parameters into request.

---
+++ src/EventSubscriber/RequestBodyJsonSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use function json_last_error;
+use function json_last_error_msg;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Class RequestBodyJsonSubscriber
+ *
+ * @package App\EventSubscriber
+ */
+class RequestBodyJsonSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'convertJsonStringToArray',
+        ];
+    }
+
+    /**
+     * @param FilterControllerEvent $event
+     */
+    public function convertJsonStringToArray(FilterControllerEvent $event)
+    {
+        $request = $event->getRequest();
+        if ($request->getContentType() != 'json' || !$request->getContent()) {
+            return;
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new BadRequestHttpException('invalid json body: ' . json_last_error_msg());
+        }
+
+        $request->request->replace(is_array($data) ? $data : []);
+    }
+}
